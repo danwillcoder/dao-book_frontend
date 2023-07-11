af7903e35afc39677950176cdf25318f2a36e69d
Will Robertson <williamdrobertson42@gmail.com>
2023-07-11 17:01:00 +1000

    feat!: Add react router scaffold

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,8 @@
-function App() {
-  return <></>;
+export default function App() {
+  return (
+    <>
+      <h1 className="font-italiana text-6xl">Dao Book</h1>
+      <p>This is a test paragraph</p>
+    </>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App.jsx';
+import './index.css';
+import PageLayout from './pages/PageLayout.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route element={<PageLayout />}>
+          <Route path="/" element={<App />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
