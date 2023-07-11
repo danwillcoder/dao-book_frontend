26d96fb1f7a0627292d66bde75bdcac4018106d8
Will Robertson <williamdrobertson42@gmail.com>
2023-07-11 15:48:23 +1000

    Initial commit
    Set up vite, tailwindcss, prettier & initialise favicon

---
+++ prettier.config.cjs
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+module.exports = {
+  trailingComma: "es5",
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  plugins: [require("prettier-plugin-tailwindcss")],
+};
