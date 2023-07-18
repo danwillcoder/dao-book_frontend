38ad15e682a577db9d0140726a741b1bed94fca6
Will Robertson <williamdrobertson42@gmail.com>
2023-07-18 20:55:17 +1000

    feat: add daobook-amber color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,9 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      colors: {
+        'daobook-amber': '#A33917',
+      },
       fontFamily: {
         italiana: 'italiana, serif',
         sans: ['libre-franklin', ...defaultTheme.fontFamily.sans],
