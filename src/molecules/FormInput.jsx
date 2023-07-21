40611dcf70060862b588580c792b6b9da6bac725
Will Robertson <williamdrobertson42@gmail.com>
2023-07-21 22:13:49 +1000

    feat: Add form input

---
+++ src/molecules/FormInput.jsx
@@ -0,0 +1,48 @@
+/**
+ @typedef formInputProps
+ @type {Object}
+ @property {'text' | 'number'} type 
+ @property {string} inputValue
+ @property {React.Dispatch<SetStateAction<string>>} setInputValue
+ @property {string} labelText 
+ @property {string} placeholderText 
+ @property {boolean} doesAutocomplete 
+ @property {boolean} isRequired 
+ */
+
+/**
+ * @param {formInputProps} props
+ */
+function FormInput({
+  type,
+  inputValue,
+  setInputValue,
+  labelText,
+  placeholderText,
+  doesAutocomplete,
+  isRequired,
+}) {
+  const handleChange = (e) => setInputValue(e.target.value);
+
+  return (
+    <>
+      <div>
+        <label className="block pb-2 text-xl">
+          {labelText}
+          {isRequired && "* (required)"}
+        </label>
+        <input
+          type={type}
+          value={inputValue}
+          onChange={handleChange}
+          placeholder={placeholderText}
+          autoComplete={doesAutocomplete}
+          required={isRequired}
+          className="rounded-2xl border-2 border-[#DFDFDF] p-2 px-4 text-xl placeholder:text-[#DFDFDF]"
+        ></input>
+      </div>
+    </>
+  );
+}
+
+export default FormInput;
