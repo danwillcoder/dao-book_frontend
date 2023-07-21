e22a7c9c462f52583193443c1d7411f5be15f960
Will Robertson <williamdrobertson42@gmail.com>
2023-07-21 21:49:26 +1000

    fix: remove propType rule & spread attr over lines

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,10 +1,11 @@
 /* eslint-disable no-undef */
 module.exports = {
-  trailingComma: 'es5',
+  trailingComma: "es5",
   tabWidth: 2,
   useTabs: false,
   semi: true,
   singleQuote: false,
-  plugins: [require('prettier-plugin-tailwindcss')],
-  tailwindConfig: './tailwind.config.cjs',
+  singleAttributePerLine: true,
+  plugins: [require("prettier-plugin-tailwindcss")],
+  tailwindConfig: "./tailwind.config.cjs",
 };
