e02cd82b6314e85c05c6d376786e15eecec4831b
Will Robertson <williamdrobertson42@gmail.com>
2023-07-21 21:57:04 +1000

    fix: add dark colour & change to double quotes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,17 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        'daobook-amber': '#A33917',
+        "daobook-amber": "#A33917",
+        "daobook-amber-dark": "#43180A",
       },
       fontFamily: {
-        italiana: 'italiana, serif',
-        sans: ['libre-franklin', ...defaultTheme.fontFamily.sans],
+        italiana: "italiana, serif",
+        sans: ["libre-franklin", ...defaultTheme.fontFamily.sans],
       },
     },
   },
