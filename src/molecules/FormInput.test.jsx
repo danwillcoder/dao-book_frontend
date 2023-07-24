0d7d5e7ce4992435c6fbd55b8beff1bb30bbbd30
Will Robertson <williamdrobertson42@gmail.com>
2023-07-24 17:19:54 +1000

    feat: add FormInput tests

---
+++ src/molecules/FormInput.test.jsx
@@ -0,0 +1,54 @@
+import { describe, it, expect, vi } from "vitest";
+import { render, screen, fireEvent } from "@testing-library/react";
+
+import FormInput from "./FormInput";
+
+describe("Form Input", () => {
+  it("shows the label text", () => {
+    render(<FormInput labelText="Test String"></FormInput>);
+    expect(screen.getByText("Test String")).toBeInTheDocument();
+  });
+
+  it("shows required if given", () => {
+    render(
+      <FormInput
+        labelText="Test String"
+        isRequired={true}
+      ></FormInput>
+    );
+    expect(screen.getByText(/required/)).toBeInTheDocument();
+  });
+
+  it("shows the correct type", () => {
+    render(
+      <FormInput
+        labelText="Test String"
+        type="number"
+        placeholderText="placeholder"
+      ></FormInput>
+    );
+    screen.debug();
+    expect(screen.getByPlaceholderText("placeholder")).toHaveAttribute("type");
+    expect(
+      screen.getByPlaceholderText("placeholder").getAttribute("type")
+    ).toBe("number");
+  });
+
+  it("calls the setInputValue function", () => {
+    const mockFn = vi.fn();
+    render(
+      <FormInput
+        labelText="Test String"
+        isRequired={true}
+        inputValue=""
+        setInputValue={mockFn}
+        placeholderText="placeholder"
+      ></FormInput>
+    );
+    fireEvent.change(screen.getByPlaceholderText("placeholder"), {
+      target: { value: "test" },
+    });
+
+    expect(mockFn).toHaveBeenCalledTimes(1);
+  });
+});
