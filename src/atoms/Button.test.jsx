3ec921a06a612cfee4e5106c98712e3518a3f79a
Will Robertson <williamdrobertson42@gmail.com>
2023-07-24 15:19:58 +1000

    fix: move tests in-line

--- src/atoms/Button.test.jsx
+++ src/atoms/Button.test.jsx
@@ -1,7 +1,7 @@
 import { describe, it, expect, vi } from "vitest";
 import { render, screen, fireEvent } from "@testing-library/react";
 
-import Button from "../src/atoms/Button.jsx";
+import Button from "./Button";
 
 describe("Button", () => {
   it("shows the button text", () => {
