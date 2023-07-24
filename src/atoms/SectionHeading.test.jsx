ede3034654ba3b3e6a06e75c9f087b2dd546b0c7
Will Robertson <williamdrobertson42@gmail.com>
2023-07-24 15:46:43 +1000

    feat: test SectionHeading

---
+++ src/atoms/SectionHeading.test.jsx
@@ -0,0 +1,30 @@
+import { describe, it, expect, vi } from "vitest";
+import { render, screen, fireEvent } from "@testing-library/react";
+
+import SectionHeading from "./SectionHeading";
+
+describe("Section Heading", () => {
+  it("shows the section text", () => {
+    render(<SectionHeading sectionText={"Test String"}></SectionHeading>);
+    expect(screen.getByText("Test String")).toBeInTheDocument();
+  });
+
+  it("shows the right tag", () => {
+    render(
+      <>
+        <SectionHeading
+          sectionText={"Test H1"}
+          isH1={true}
+        ></SectionHeading>
+        <SectionHeading
+          sectionText={"Test Paragraph"}
+          isH1={false}
+        ></SectionHeading>
+      </>
+    );
+    const h1 = screen.getByText("Test H1");
+    const paragraph = screen.getByText("Test Paragraph");
+    expect(h1.tagName).toBe("H1");
+    expect(paragraph.tagName).toBe("P");
+  });
+});
