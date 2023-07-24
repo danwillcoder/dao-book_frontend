b62c38132c25237897722d3b6f19132369d63b10
Will Robertson <williamdrobertson42@gmail.com>
2023-07-24 20:01:43 +1000

    feat: remove unneeded footer

--- src/pages/PageLayout.jsx
+++ src/pages/PageLayout.jsx
@@ -1,5 +1,4 @@
 import { Outlet } from "react-router-dom";
-import Footer from "../components/Footer";
 import Header from "../components/Header";
 
 function PageLayout() {
@@ -7,7 +6,6 @@ function PageLayout() {
     <>
       <Header />
       <Outlet />
-      <Footer />
     </>
   );
 }
