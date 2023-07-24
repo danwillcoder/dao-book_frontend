9c2fa9b85e5cf3acbcae4b208d496a4c9562d398
Will Robertson <williamdrobertson42@gmail.com>
2023-07-24 17:01:54 +1000

    feat: add Title Lockup tests

---
+++ src/atoms/TitleLockup.test.jsx
@@ -0,0 +1,36 @@
+import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+
+import TitleLockup from "./TitleLockup";
+
+describe("Title Lockup", () => {
+  it("shows the title text", () => {
+    render(<TitleLockup></TitleLockup>);
+    expect(screen.getByText("Dao Book")).toBeInTheDocument();
+  });
+
+  it("shows the light theme", () => {
+    render(
+      <TitleLockup
+        theme="light"
+        isSubtitled={true}
+      ></TitleLockup>
+    );
+    expect(screen.getByText("Dao Book")).toHaveClass("text-white");
+  });
+
+  it("shows the dark theme", () => {
+    render(
+      <TitleLockup
+        theme="dark"
+        isSubtitled={true}
+      ></TitleLockup>
+    );
+    expect(screen.getByText("Dao Book")).toHaveClass("text-black");
+  });
+
+  it("shows the subtitle", () => {
+    render(<TitleLockup isSubtitled={true}></TitleLockup>);
+    expect(screen.getByText("The way of clinic notes")).toBeInTheDocument();
+  });
+});
