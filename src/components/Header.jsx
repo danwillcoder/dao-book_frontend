d6db462479d3a03dca39f56dc9a83798029e5bb2
Will Robertson <williamdrobertson42@gmail.com>
2023-07-24 17:47:15 +1000

    feat: add header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,46 @@
+import { useLocation, useNavigate } from "react-router-dom";
+import Button from "../atoms/Button";
+import TitleLockup from "../atoms/TitleLockup";
+
 function Header() {
-  return <h1>Header</h1>;
+  const location = useLocation();
+  const navigate = useNavigate();
+  //TODO: Fix logout button when login added
+  const isLoggedIn = true;
+  const dashboardRoute = "/dashboard";
+
+  const isNotOnDashboard = location.pathname !== dashboardRoute;
+
+  return (
+    <header className="flex place-content-between items-center bg-daobook-amber p-6">
+      <TitleLockup
+        theme="light"
+        isSubtitled={false}
+        isSmall={true}
+      ></TitleLockup>
+      <div className="inline-flex gap-7">
+        {isNotOnDashboard && (
+          <Button
+            theme="inverted"
+            buttonText="Dashboard"
+            onClick={() => {
+              navigate(dashboardRoute);
+            }}
+          ></Button>
+        )}
+        {isLoggedIn && (
+          <Button
+            theme="light"
+            buttonText="Logout"
+            onClick={() => {
+              // logout here
+              console.log("Logged out");
+            }}
+          ></Button>
+        )}
+      </div>
+    </header>
+  );
 }
 
 export default Header;
