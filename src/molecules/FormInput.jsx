46e72c09cbd79ee3a8f0e7d3e173005bf80c3f57
Will Robertson <williamdrobertson42@gmail.com>
2023-07-26 17:51:53 +1000

    feat!: update input props and add checkbox type

--- src/molecules/FormInput.jsx
+++ src/molecules/FormInput.jsx
@@ -1,7 +1,7 @@
 /**
  @typedef formInputProps
  @type {Object}
- @property {'text' | 'number' | 'email' | 'password'} type 
+ @property {'text' | 'number' | 'email' | 'password' | 'date' | 'textarea' | 'checkbox'} type 
  @property {React.Dispatch<SetStateAction<string>>} setInputValue
  @property {string} labelText 
  @property {string} placeholderText 
@@ -20,22 +20,67 @@ function FormInput({
   placeholderText,
   doesAutocomplete,
   isRequired,
+  defaultValue,
+  defaultChecked,
 }) {
+  const formIsTextArea = type === "textArea";
+  const formIsCheckbox = type === "checkbox";
+
+  if (formIsCheckbox) {
+    return (
+      <div className="flex items-center gap-4">
+        <input
+          type={type}
+          id={name}
+          name={name}
+          onChange={onChange}
+          defaultChecked={defaultChecked}
+          className="h-4 w-4 border-[#DFDFDF]"
+        ></input>
+        <label
+          className="text-xl"
+          htmlFor={name}
+        >
+          {labelText}
+        </label>
+      </div>
+    );
+  }
+
   return (
     <div>
-      <label className="block pb-2 text-xl">
+      <label
+        className="block pb-2 text-xl"
+        htmlFor={name}
+      >
         {labelText}
         {isRequired && "* (required)"}
       </label>
-      <input
-        type={type}
-        name={name}
-        onChange={onChange}
-        placeholder={placeholderText}
-        autoComplete={doesAutocomplete}
-        required={isRequired}
-        className="w-full rounded-2xl border-2 border-[#DFDFDF] p-2 px-4 text-xl placeholder:text-[#DFDFDF]"
-      ></input>
+      {formIsTextArea ? (
+        <textarea
+          type={type}
+          id={name}
+          name={name}
+          onChange={onChange}
+          placeholder={placeholderText}
+          autoComplete={doesAutocomplete}
+          required={isRequired}
+          defaultValue={defaultValue}
+          className="w-full rounded-2xl border-2 border-[#DFDFDF] p-2 px-4 text-xl placeholder:text-[#DFDFDF]"
+        ></textarea>
+      ) : (
+        <input
+          type={type}
+          id={name}
+          name={name}
+          onChange={onChange}
+          placeholder={placeholderText}
+          autoComplete={doesAutocomplete}
+          required={isRequired}
+          defaultValue={defaultValue}
+          className="w-full rounded-2xl border-2 border-[#DFDFDF] p-2 px-4 text-xl placeholder:text-[#DFDFDF]"
+        ></input>
+      )}
     </div>
   );
 }
