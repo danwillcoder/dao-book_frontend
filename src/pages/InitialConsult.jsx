82587d45ff03b49fc18b9756a9582cc71558173e
Will Robertson <williamdrobertson42@gmail.com>
2023-07-26 17:49:31 +1000

    feat: add initial consult form

--- src/pages/InitialConsult.jsx
+++ src/pages/InitialConsult.jsx
@@ -1,15 +1,31 @@
 import { useState } from "react";
 import Button from "../atoms/Button";
 import FormInput from "../molecules/FormInput";
+import TextLink from "../atoms/TextLink";
+import { Form } from "react-router-dom";
 
 function InitialConsult() {
   const [formData, setFormData] = useState({
     firstName: "",
     lastName: "",
     email: "",
-    AHPRA: 0,
-    password: "",
+    dateOfBirth: "",
+    phone: "",
+    medications: "",
+    history: "",
+    sessionDate: "",
+    complaint: "",
+    sessionNotes: "",
+    tongueNotes: "",
+    pulseNotes: "",
+    formulaName: "",
+    formulaComposition: "",
+    formulaDosage: "",
+    lifestyleNotes: "",
+    sendEmail: false,
   });
+
+  const [isSaved, setHasSaved] = useState();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -17,23 +33,27 @@ function InitialConsult() {
   };
 
   const handleChange = (e) => {
+    const type = e.target.type;
     const name = e.target.name;
-    const value = e.target.value;
+    const value = type === "checkbox" ? e.target.checked : e.target.value;
     setFormData((previousData) => ({
       ...previousData,
       [name]: value,
     }));
   };
 
+  const defaultDOBValue = "1990-01-01";
+  const todayDateNoTime = new Date().toISOString().split("T")[0];
+
   return (
     <div className="flex flex-col">
       <h1 className="my-20 text-center text-4xl">Initial Consult Form</h1>
       <div className="flex flex-col flex-wrap content-center justify-center gap-4">
-        <h1 className="w-[80%] text-4xl">Patient Profile</h1>
         <form
           className="px-15 flex max-w-2xl flex-col gap-4"
           onSubmit={handleSubmit}
         >
+          <h1 className="w-[700px] text-4xl">Patient Profile</h1>
           <FormInput
             type="text"
             name="firstName"
@@ -64,6 +84,7 @@ function InitialConsult() {
             labelText="Date of Birth"
             isRequired={true}
             onChange={handleChange}
+            defaultValue={defaultDOBValue}
           ></FormInput>
           <FormInput
             type="text"
@@ -86,12 +107,87 @@ function InitialConsult() {
             placeholderText=""
             onChange={handleChange}
           ></FormInput>
-          <Button
-            theme="light"
-            isFullWidth={true}
-            buttonText="Register"
-          ></Button>
+          <hr className="my-20" />
+          <FormInput
+            type="date"
+            name="sessionDate"
+            labelText="Session date"
+            onChange={handleChange}
+            defaultValue={todayDateNoTime}
+          ></FormInput>
+          <FormInput
+            type="text"
+            name="complaint"
+            labelText="Main complaint"
+            onChange={handleChange}
+          ></FormInput>
+          <FormInput
+            type="textArea"
+            name="sessionNotes"
+            labelText="Session notes"
+            onChange={handleChange}
+          ></FormInput>
+          <FormInput
+            type="textArea"
+            name="tongueNotes"
+            labelText="Tongue"
+            onChange={handleChange}
+          ></FormInput>
+          <FormInput
+            type="textArea"
+            name="pulseNotes"
+            labelText="Pulse"
+            onChange={handleChange}
+          ></FormInput>
+          <hr className="my-10" />
+          <h2 className="w-[700px] text-3xl">Prescription</h2>
+          <FormInput
+            type="text"
+            name="formulaName"
+            labelText="Formula name"
+            onChange={handleChange}
+          ></FormInput>
+          <FormInput
+            type="textArea"
+            name="formulaComposition"
+            labelText="Composition"
+            onChange={handleChange}
+          ></FormInput>
+          <FormInput
+            type="textArea"
+            name="formulaDosage"
+            labelText="Dosage & administration"
+            onChange={handleChange}
+          ></FormInput>
+          <FormInput
+            type="textArea"
+            name="lifestyleNotes"
+            labelText="Lifestyle notes"
+            onChange={handleChange}
+          ></FormInput>
+          <FormInput
+            type="checkbox"
+            name="sendEmail"
+            labelText="Email prescription & lifestyle notes"
+            onChange={handleChange}
+          ></FormInput>
+          {!isSaved && (
+            <Button
+              theme="light"
+              isFullWidth={true}
+              buttonText="Save"
+            ></Button>
+          )}
         </form>
+        {isSaved ? (
+          <TextLink
+            linkText={"Return to Dashboard"}
+            linkDestination={"/"}
+            className={"mb-10 text-center"}
+          ></TextLink>
+        ) : (
+          <div className="mb-10"></div>
+        )}
       </div>
     </div>
   );
