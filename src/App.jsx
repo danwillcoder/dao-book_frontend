123897206d274f94d29752886eef685a39d813dc
Will Robertson <williamdrobertson42@gmail.com>
2023-07-29 14:32:54 +1000

    feat: add navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import DashboardButton from "./atoms/DashboardButton";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function App() {
   //TODO get user's first name from context
   const [userName, setUserName] = useState("");
+  const navigate = useNavigate();
+
+  const initialConsultClick = () => {
+    navigate("/initial-consult");
+  };
+
+  const returnConsultClick = () => {
+    navigate("/return-consult");
+  };
+
+  const patientListClick = () => {
+    navigate("/patient-list");
+  };
 
   return (
     <div className="mt-20 flex flex-col items-center gap-7">
@@ -13,9 +27,18 @@ export default function App() {
       </h1>
       <div className="flex gap-7">
         {/* TODO add the page navigation */}
-        <DashboardButton buttonText="Initial Consult" />
-        <DashboardButton buttonText="Return Consult" />
-        <DashboardButton buttonText="Patient List" />
+        <DashboardButton
+          buttonText="Initial Consult"
+          onClick={initialConsultClick}
+        />
+        <DashboardButton
+          buttonText="Return Consult"
+          onClick={returnConsultClick}
+        />
+        <DashboardButton
+          buttonText="Patient List"
+          onClick={patientListClick}
+        />
       </div>
     </div>
   );
