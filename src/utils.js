62fce4991e1ced6cf9b7757260b850acff1f6f31
Will Robertson <williamdrobertson42@gmail.com>
2023-07-30 15:45:09 +1000

    feat: functions for interacting with JWTs

---
+++ src/utils.js
@@ -0,0 +1,23 @@
+function parseJwt(token) {
+  const base64Url = token.split(".")[1];
+  const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  const jsonPayload = decodeURIComponent(
+    window
+      .atob(base64)
+      .split("")
+      .map(function (c) {
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join("")
+  );
+
+  return JSON.parse(jsonPayload);
+}
+
+function isJwtExpired(decodedToken) {
+  const JwtTimestampInSeconds = decodedToken.exp;
+  const timeNowAsEpochSecondTimestamp = Math.trunc(new Date().getTime() / 1000);
+  return timeNowAsEpochSecondTimestamp > JwtTimestampInSeconds;
+}
+
+export { parseJwt, isJwtExpired };
