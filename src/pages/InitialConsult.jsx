80dc8071610c79e2c5aa8d415071aa20420368fb
Will Robertson <williamdrobertson42@gmail.com>
2023-08-01 10:56:34 +1000

    feat: post session data from consult form

--- src/pages/InitialConsult.jsx
+++ src/pages/InitialConsult.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
-import { useLocation } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
+import { axiosInstance, sessionRoutes } from "../api/routes";
 import ConsultForm from "../components/ConsultForm";
-import { Navigate } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
 
 function InitialConsult() {
   const todaysDate = new Date().toISOString().split("T")[0];
@@ -17,18 +18,45 @@ function InitialConsult() {
     lifestyleAdvice: "",
     sendEmail: false,
   });
+  const [error, setError] = useState({});
 
-  const locationState = useLocation().state;
-  console.log(locationState);
+  const { patientId } = useLocation().state;
+  const { token } = useAuth();
 
-  // TODO: submit to backend and update state accordingly (setHasSaved)
-  // Does this also become live data they can update & resubmit?
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(JSON.stringify(formData));
+
+    // Unpack into two objects as we send 2x requests
+    const sessionData = {
+      patientId,
+      complaint: formData.complaint,
+      sessionNotes: formData.sessionNotes,
+      tongueNotes: formData.tongueNotes,
+      pulseNotes: formData.pulseNotes,
+    };
+    const prescriptionData = {
+      formulaName: formData.formulaName,
+      composition: formData.composition,
+      dosageAdministration: formData.dosageAdministration,
+      lifestyleAdvice: formData.lifestyleAdvice,
+    };
+
+    try {
+      const res = await axiosInstance.post(sessionRoutes.create, sessionData, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+
+      console.log(res);
+    } catch (error) {
+      setError({
+        status: error.response.status,
+        message: error.response.data.message,
+      });
+    }
   };
 
   const handleChange = (e) => {
+    setError(null);
     const type = e.target.type;
     const name = e.target.name;
     const value = type === "checkbox" ? e.target.checked : e.target.value;
@@ -39,7 +67,7 @@ function InitialConsult() {
   };
 
   // Can only reach page from initial-patient page
-  if (!locationState) {
+  if (!patientId) {
     return <Navigate to="/" />;
   }
 
@@ -52,6 +80,7 @@ function InitialConsult() {
         handleSubmit={handleSubmit}
         isInitialConsult={true}
       />
+      {error && <p>{error.message}</p>}
     </div>
   );
 }
