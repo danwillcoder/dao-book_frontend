1df07d1dbbccc284d58cc8c94e65e68ffca7388c
Will Robertson <williamdrobertson42@gmail.com>
2023-08-01 21:29:09 +1000

    feat: build out edit route
    This is still not working due to backend errors

--- src/pages/ReturnConsult.jsx
+++ src/pages/ReturnConsult.jsx
@@ -9,6 +9,7 @@ import ConsultForm from "../components/ConsultForm";
 import useErrorHandler from "../hooks/errorHandler";
 import useAuth from "../hooks/useAuth";
 import { useLocation } from "react-router-dom/dist";
+import { dateTimeToDate } from "../utils";
 
 function ReturnConsult() {
   // Hooks
@@ -17,16 +18,18 @@ function ReturnConsult() {
 
   // If we arrived here from patientList, we should have a patient already active
   const location = useLocation();
-  let selectedPatientId = location.state?.selectedPatientId;
+  const selectedPatientId = location.state?.selectedPatientId;
+  const selectedSessionId = location.state?.selectedSessionId;
 
   // State
   const [formData, setFormData] = useState({
     patientId: selectedPatientId || "",
+    sessionId: selectedSessionId || "",
     sessionDate: "",
     complaint: "",
     sessionNotes: "",
-    tongueNotes: "",
-    pulseNotes: "",
+    tongue: "",
+    pulse: "",
     formulaName: "",
     composition: "",
     dosageAdministration: "",
@@ -34,8 +37,10 @@ function ReturnConsult() {
     sendEmail: false,
   });
   const [patients, setPatients] = useState([]);
+  const [activePatient, setActivePatient] = useState(null);
   const [error, setError] = useState({});
 
+  // Fetch initial data
   useEffect(() => {
     const fetchPatients = async () => {
       try {
@@ -43,28 +48,83 @@ function ReturnConsult() {
           headers: { Authorization: `Basic ${token}` },
         });
         setPatients(res.data.patients);
+
+        // If this exists, we're editing, so we need to set the name and hide the select
+        if (selectedPatientId) {
+          setActivePatient(
+            res.data.patients.find((patient) => {
+              return patient._id === selectedPatientId;
+            })
+          );
+        }
       } catch (error) {
         errorHandler(error, setError);
       }
     };
+
+    // If we're editing, we need to get previous data
+    const fetchSessionDetails = async () => {
+      try {
+        const sessionRes = await axiosInstance.get(
+          sessionRoutes.getOne + selectedSessionId,
+          {
+            headers: { Authorization: `Basic ${token}` },
+          }
+        );
+        const returnedSessionData = sessionRes.data?.session;
+        console.log(selectedSessionId);
+
+        const prescriptionRes = await axiosInstance.get(
+          prescriptionRoutes.getOne + selectedSessionId,
+          {
+            headers: { Authorization: `Basic ${token}` },
+          }
+        );
+        const returnedPrescriptionData = sessionRes.data?.session;
+
+        console.log(returnedPrescriptionData);
+        // const prescriptionRes = await axiosInstance.get(prescriptionRoutes.get);
+        //TODO Fetch prescription data and map over the same as below
+        const newSessionData = {};
+        const dataKeys = Object.keys(formData);
+        for (const key of dataKeys) {
+          newSessionData[key] = returnedSessionData[key];
+        }
+
+        setFormData({
+          ...formData,
+          ...newSessionData,
+        });
+      } catch (error) {
+        console.log(error);
+        errorHandler(error, setError);
+      }
+    };
+
     fetchPatients();
+    // If there is a session, we need to fetch session details. We're editing
+    if (selectedSessionId) {
+      fetchSessionDetails();
+    }
   }, []);
 
+  // Submit form handler
   const handleSubmit = async (e) => {
     e.preventDefault();
     setError(null);
 
     // Unpack into two objects as we send 2x requests
     const sessionData = {
-      patientId: activePatient,
+      patientId: formData.patientId,
       complaint: formData.complaint,
       sessionNotes: formData.sessionNotes,
-      tongueNotes: formData.tongueNotes,
-      pulseNotes: formData.pulseNotes,
+      tongue: formData.tongue,
+      pulse: formData.pulse,
     };
 
     const prescriptionData = {
-      patientId: activePatient,
+      patientId: formData.patientId,
+      sessionId: formData.sessionId || "",
       formulaName: formData.formulaName,
       composition: formData.composition,
       dosageAdministration: formData.dosageAdministration,
@@ -72,26 +132,49 @@ function ReturnConsult() {
       sendEmail: formData.sendEmail,
     };
 
-    try {
-      const sessionRes = await axiosInstance.post(
-        sessionRoutes.create,
-        sessionData,
-        {
-          headers: { Authorization: `Bearer ${token}` },
-        }
-      );
+    // If there's a session, we're editing, so put requests
+    if (selectedSessionId) {
+      try {
+        const sessionRes = await axiosInstance.put(
+          sessionRoutes.put + formData.sessionId,
+          sessionData,
+          {
+            headers: { Authorization: `Bearer ${token}` },
+          }
+        );
 
-      // Use returned sessionId to send prescription
-      const sessionId = sessionRes.data.session._id;
-      prescriptionData.sessionId = sessionId;
+        const prescriptionRes = await axiosInstance.put(
+          // Update with prescriptionId from session
+          prescriptionRoutes.put,
+          prescriptionData,
+          { headers: { Authorization: `Bearer ${token}` } }
+        );
+      } catch (error) {
+        errorHandler(error, setError);
+      }
+    } else {
+      // If there's no session, this is a new session, so we post
+      try {
+        const sessionRes = await axiosInstance.post(
+          sessionRoutes.create,
+          sessionData,
+          {
+            headers: { Authorization: `Bearer ${token}` },
+          }
+        );
 
-      const prescriptionRes = await axiosInstance.post(
-        prescriptionRoutes.create,
-        prescriptionData,
-        { headers: { Authorization: `Bearer ${token}` } }
-      );
-    } catch (error) {
-      errorHandler(error, setError);
+        // Use returned sessionId to send prescription
+        const sessionId = sessionRes.data.session._id;
+        prescriptionData.sessionId = sessionId;
+
+        const prescriptionRes = await axiosInstance.post(
+          prescriptionRoutes.create,
+          prescriptionData,
+          { headers: { Authorization: `Bearer ${token}` } }
+        );
+      } catch (error) {
+        errorHandler(error, setError);
+      }
     }
   };
 
@@ -109,23 +192,31 @@ function ReturnConsult() {
     <div className="flex flex-col">
       <h1 className="my-20 text-center text-4xl">Return Consult Form</h1>
       <div className="px-15 min-w-[500px] gap-4 text-center">
-        <label htmlFor="patientName text-xl">Patient: </label>
-        <select
-          name="patientId"
-          id="patientId"
-          className="rounded-2xl border-2 border-[#DFDFDF] p-2 px-4 text-xl placeholder:text-[#DFDFDF]"
-          onChange={handleChange}
-          value={formData.patientId}
-        >
-          {patients.map((patient) => (
-            <option
-              value={patient._id}
-              key={patient._id}
+        {activePatient ? (
+          <h1 className="text-center text-2xl">
+            {activePatient.firstName} {activePatient.lastName}
+          </h1>
+        ) : (
+          <>
+            <label htmlFor="patientName text-xl">Patient: </label>
+            <select
+              name="patientId"
+              id="patientId"
+              className="rounded-2xl border-2 border-[#DFDFDF] p-2 px-4 text-xl placeholder:text-[#DFDFDF]"
+              onChange={handleChange}
+              value={formData.patientId}
             >
-              {patient.firstName} {patient.lastName}
-            </option>
-          ))}
-        </select>
+              {patients.map((patient) => (
+                <option
+                  value={patient._id}
+                  key={patient._id}
+                >
+                  {patient.firstName} {patient.lastName}
+                </option>
+              ))}
+            </select>
+          </>
+        )}
       </div>
       <ConsultForm
         formData={formData}
