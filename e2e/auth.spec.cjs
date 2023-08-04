585ef22def421c500654099cecad1969a8b3ae34
Will Robertson <williamdrobertson42@gmail.com>
2023-08-04 11:15:33 +1000

    feat: create update JWT function

--- e2e/auth.spec.cjs
+++ e2e/auth.spec.cjs
@@ -1,6 +1,31 @@
 const { test, expect } = require("@playwright/test");
 
+/**
+ *
+ * @param {string} originalToken A complete JWT token
+ * @param {number} newExpiration
+ * @returns JWT with updated timestamp
+ */
+function updateJwtExpiration(originalToken, newExpiration) {
+  const [headerBase64, payloadBase64, signatureBase64] =
+    originalToken.split(".");
+
+  const payload = JSON.parse(atob(payloadBase64));
+  payload.exp = newExpiration;
+  const updatedPayloadBase64 = btoa(JSON.stringify(payload));
+
+  const updatedToken = `${headerBase64}.${updatedPayloadBase64}.${signatureBase64}`;
+
+  return updatedToken;
+}
+
+module.exports = {
+  updateJwtExpiration,
+};
+
 test.describe("Register", () => {
+  const fakePracId = "64c795c2b4524e92922cdc9b";
+
   test.beforeEach(async ({ page }) => {
     await page.goto("register");
     await page.route("**/*/prac", async (route) => {
@@ -9,10 +34,10 @@ test.describe("Register", () => {
         prac: {
           firstName: "TestName",
           lastName: "TestLast",
-          email: "test@test.com",
+          email: "testEmail@example.com",
           password:
             "$2a$10$ApcuKTdzYGpyiXe3BCcy5uvlIUtAzGGzXjyO3u4nfGWAt8wzOVdUu",
-          _id: "64c795c2b4524e92922cdc9b",
+          _id: fakePracId,
           __v: 0,
         },
       };
@@ -58,10 +83,16 @@ test.describe("Register", () => {
 test.describe("Login", () => {
   test.beforeEach(async ({ page }) => {
     await page.goto("login");
+
+    const originalToken =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGM3MGJhNzBjNmY0MDMxOTliYWMyZWMiLCJpYXQiOjE2OTExMTAxMjEsImV4cCI6MTY5MTExMzcyMX0.fVH8aN5RF27WqBzOv_lMuq7vQxgvfIRZIMUGfYHmzOg";
+    const newExpiration = Math.floor(Date.now() / 1000) + 60 * 60 * 24; // New expiration time (e.g., 24 hours from now)
+    const updatedToken = updateJwtExpiration(originalToken, newExpiration);
+
     await page.route("**/*/prac/login", async (route) => {
+      // This login token expires every hour.
       const loginJson = {
-        token:
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGM3OTVjMmI0NTI0ZTkyOTIyY2RjOWIiLCJpYXQiOjE2OTA4MDE2NTksImV4cCI6MTY5MDgwNTI1OX0.8Tu3H2eNcx4ez3Mc1UKd2kMz4IZNgcloDA4BFIBuKzQ",
+        token: updatedToken,
       };
       await route.fulfill({ body: JSON.stringify(loginJson) });
     });
@@ -95,8 +126,9 @@ test.describe("Login", () => {
       .fill("testEmail@example.com");
     await page.getByPlaceholder("*****").fill("daobook");
     await page.getByRole("button", { name: "Login" }).click();
-    await page.waitForURL("/");
-    expect(await page.getByRole("heading").count()).toBe(1);
+    await expect(
+      await page.getByRole("heading", { name: "Welcome to your clinic" })
+    ).toBeVisible();
   });
 
   test("should not allow me to login with missing fields", async ({ page }) => {
