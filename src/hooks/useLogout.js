f2e80d88f5877b70ac462cee124edd253b30dd03
Will Robertson <williamdrobertson42@gmail.com>
2023-08-04 10:15:35 +1000

    fix: navigate on logout

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,12 +1,15 @@
 import useAuth from "../hooks/useAuth";
+import { useNavigate } from "react-router-dom";
 
 const useLogout = () => {
   const { setAuth, setToken, setPracName } = useAuth();
+  const navigate = useNavigate();
   return () => {
     localStorage.clear();
     setAuth({});
     setToken({});
     setPracName({});
+    navigate("/login");
   };
 };
 
