d77b0d1048f4661d1ef732baf18a06c995aef9b8
Will Robertson <williamdrobertson42@gmail.com>
2023-08-04 12:11:12 +1000

    fix: prevent map error without patient list

--- src/pages/PatientList.jsx
+++ src/pages/PatientList.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import Button from "../atoms/Button";
 import { axiosInstance, patientRoutes } from "../api/routes";
 import useAuth from "../hooks/useAuth";
 
@@ -28,21 +27,22 @@ function PatientList() {
         Patient List for {pracName ? `Dr. ${pracName}.` : "Doctor."}
       </h1>
       <ul className="flex flex-col items-center gap-7">
-        {patients.map((patient) => {
-          return (
-            <li
-              className="min-w-[600px]"
-              key={patient._id}
-            >
-              <Link
-                to={`/patient-list/${patient._id}`}
-                className="block w-full rounded-2xl border-4 border-[#E3E3E3] bg-white px-2 py-6 text-center font-sans font-semibold text-black shadow-md transition-colors transition-transform hover:scale-105 hover:bg-black/5 focus:ring dark:bg-black/50 dark:text-white dark:hover:bg-black/70"
+        {patients &&
+          patients.map((patient) => {
+            return (
+              <li
+                className="min-w-[600px]"
+                key={patient._id}
               >
-                {patient.firstName} {patient.lastName}
-              </Link>
-            </li>
-          );
-        })}
+                <Link
+                  to={`/patient-list/${patient._id}`}
+                  className="block w-full rounded-2xl border-4 border-[#E3E3E3] bg-white px-2 py-6 text-center font-sans font-semibold text-black shadow-md transition-colors transition-transform hover:scale-105 hover:bg-black/5 focus:ring dark:bg-black/50 dark:text-white dark:hover:bg-black/70"
+                >
+                  {patient.firstName} {patient.lastName}
+                </Link>
+              </li>
+            );
+          })}
       </ul>
     </div>
   );
